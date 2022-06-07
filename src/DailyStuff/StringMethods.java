a64dfd1cb8a2f97ce28149f3b78a89f97ae29bc4
Shukur A <sukuri33@gmail.com>
2022-06-07 12:03:19 -0500

    committing all

---
+++ src/DailyStuff/StringMethods.java
@@ -0,0 +1,20 @@
+package DailyStuff;
+
+import java.util.Arrays;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        
+        String str = "I love Java";
+        String[] strArr = str.split(" ");
+        String reversedStr = "";
+        for (int i = strArr.length - 1, j = 0 ; i >= 0; i-- , j++) {
+
+            reversedStr += strArr[i]+" ";
+        }
+
+        System.out.println(reversedStr.stripTrailing()); // gets rid off trailing space from
+                                                            // string value.
+        
+    }
+}
