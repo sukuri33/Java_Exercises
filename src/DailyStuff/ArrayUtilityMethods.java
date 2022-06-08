bb2d1222bc592693276904f22b9059c2e43e6a49
Shukur A <sukuri33@gmail.com>
2022-06-07 19:54:04 -0500

    committing all

---
+++ src/DailyStuff/ArrayUtilityMethods.java
@@ -0,0 +1,18 @@
+package DailyStuff;
+
+import java.util.Arrays;
+
+public class ArrayUtilityMethods {
+
+
+    public static void main(String[] args) {
+
+
+        int[] score = {70,90,100,80,65};
+
+        System.out.println(Arrays.toString(score));
+
+    }
+
+
+}
