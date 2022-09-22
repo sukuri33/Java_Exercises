d0b0097a086e61a8fc1a2ef6a95b8628fab5b1e3
Shukur A <sukuri33@gmail.com>
2022-09-22 17:20:29 -0500

    Committing date conversion Java Code Challenge

--- src/Day24_Assignments_DateRelated/DateExplore.java
+++ src/Day24_Assignments_DateRelated/DateExplore.java
@@ -15,6 +15,14 @@ public class DateExplore {
         DateTimeFormatter df = DateTimeFormatter.ofPattern("EEEE, h:mm a, MMM/dd/y");
         System.out.println(localDateTime.format(df));
 
+
+        String myD = "From Date:Sat Jan 01 00:00:00 CST 2022";
+        String str = myD.substring(myD.indexOf(":")+1);
+        LocalDateTime parse = LocalDateTime.parse(str, DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy"));
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String format = parse.format(dateTimeFormatter);
+        System.out.println(format);
+
     }
 
 }
